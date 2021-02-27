e00d0fe49ec98f0acc1afab71e220ab7e29abc38
Bintang <kadekbintanga@gmail.com>
2021-02-27 18:01:09 +0800

    Create File Play List And Model Mock Up Data

--- angular-c2/src/app/app.module.ts
+++ angular-c2/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-c2/src/app/model/play-list.model.spec.ts
@@ -0,0 +1,7 @@
+import {PlayList.Model} from './play-list.model';
+
+describe('PlayList.Model', () => {
+  it('should create an instance', () => {
+    expect(new PlayList.Model()).toBeTruthy();
+  });
+});
